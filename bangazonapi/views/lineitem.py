81415df26c43f8a26859d379ed78f41ed3861fcb
Val Currie <contact@va.lcuri.email>
2021-08-31 09:34:29 -0500

    fixed pk in lineitem ORM get and added a call to .delete() on it

--- bangazonapi/views/lineitem.py
+++ bangazonapi/views/lineitem.py
@@ -80,7 +80,8 @@ class LineItems(ViewSet):
         try:
             customer = Customer.objects.get(user=request.auth.user)
             order_product = OrderProduct.objects.get(
-                product__id=pk, order__customer=customer)
+                pk=pk, order__customer=customer)
+            order_product.delete()
 
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
